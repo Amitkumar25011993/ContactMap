6f7ab21552797bb1952af0e485ded1af07458140
amitkumar25011993 <amitkumar25011993@gmail.com>
2017-01-13 16:21:31 +0530

    first commit

---
+++ app/src/main/java/com/amitkumar/contactsmap/SinglePersonActivity.java
@@ -0,0 +1,37 @@
+package com.amitkumar.contactsmap;
+
+/**
+ * Created by AmitKumar on 1/13/2017.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class SinglePersonActivity extends Activity {
+
+    private static final String TAG_NAME = "name";
+    private static final String TAG_EMAIL = "email";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_person);
+
+        Intent in = getIntent();
+
+        String name = in.getStringExtra(TAG_NAME);
+        String email = in.getStringExtra(TAG_EMAIL);
+
+
+        TextView lblName = (TextView) findViewById(R.id.name_label);
+        TextView lblEmail = (TextView) findViewById(R.id.email_label);
+
+
+        lblName.setText(name);
+        lblEmail.setText(email);
+    }
+}
+
